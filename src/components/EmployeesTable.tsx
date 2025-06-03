43ea0b8309cd20bd0c501ae96b801fa330016d52
VladislavAduenko <adyenko.job@gmail.com>
2025-06-04 00:23:49 +0300

    add functionality of deleting employee

--- src/components/EmployeesTable.tsx
+++ src/components/EmployeesTable.tsx
@@ -1,13 +1,15 @@
-import { Avatar, Box, Spinner, Table, Text } from "@chakra-ui/react";
+import { Avatar, Box, Button, Spinner, Table, Text } from "@chakra-ui/react";
 import useEmployees from "../hooks/useEmployees";
 import { QueryFunction } from "@tanstack/react-query";
 import Employee from "../model/Employee";
 import { FC } from "react";
 import DepartmentSelector from "./DepartmentSelector";
 interface Props {
-  queryFn: QueryFunction<Employee[]>
+  queryFn: QueryFunction<Employee[]>;
+  deleteEmployee: (id: string) => void;
+  isDeleting?: boolean;
 }
-const EmployeesTable: FC<Props> = ({queryFn}) => {
+const EmployeesTable: FC<Props> = ({ queryFn, deleteEmployee, isDeleting }) => {
   const { data: employees, error, isLoading } = useEmployees(queryFn);
   return (
     <>
@@ -16,24 +18,31 @@ const EmployeesTable: FC<Props> = ({queryFn}) => {
       ) : (
         <>
           {isLoading && <Spinner></Spinner>}
-          <Box marginLeft={"30vw"} marginBottom = "2vh">
+          <Box marginLeft={"30vw"} marginBottom="2vh">
             <DepartmentSelector></DepartmentSelector>
           </Box>
-          <Table.ScrollArea borderWidth="1px" rounded="md" height="50vh" >
+          <Table.ScrollArea borderWidth="1px" rounded="md" height="50vh">
             <Table.Root size="sm" stickyHeader className="table">
-              <Table.Header fontSize={{sm:"1.1rem",md:"1.2"}}>
+              <Table.Header fontSize={{ sm: "1.1rem", md: "1.2" }}>
                 <Table.Row bg="bg.subtle" zIndex={"auto"}>
                   <Table.ColumnHeader hideBelow={"md"}></Table.ColumnHeader>
                   <Table.ColumnHeader>Name</Table.ColumnHeader>
                   <Table.ColumnHeader>Department</Table.ColumnHeader>
-                  <Table.ColumnHeader  hideBelow={"sm"} >Salary</Table.ColumnHeader>
-                  <Table.ColumnHeader  hideBelow={"sm"} >Birthdate</Table.ColumnHeader>
+                  <Table.ColumnHeader hideBelow={"sm"}>
+                    Salary
+                  </Table.ColumnHeader>
+                  <Table.ColumnHeader hideBelow={"sm"}>
+                    Birthdate
+                  </Table.ColumnHeader>
+                  <Table.ColumnHeader hideBelow={"sm"}>
+                    Birthdate
+                  </Table.ColumnHeader>
                 </Table.Row>
               </Table.Header>
               <Table.Body>
                 {employees?.map((empl) => (
-                  <Table.Row key={empl.id} >
-                    <Table.Cell  hideBelow={"md"}>
+                  <Table.Row key={empl.id}>
+                    <Table.Cell hideBelow={"md"}>
                       <Avatar.Root>
                         <Avatar.Fallback name={empl.fullName} />
                         <Avatar.Image src={empl.avatar} />
@@ -41,8 +50,19 @@ const EmployeesTable: FC<Props> = ({queryFn}) => {
                     </Table.Cell>
                     <Table.Cell>{empl.fullName}</Table.Cell>
                     <Table.Cell>{empl.department}</Table.Cell>
-                    <Table.Cell  hideBelow={"sm"}>{empl.salary}</Table.Cell>
-                    <Table.Cell  hideBelow={"sm"}>{empl.birthDate}</Table.Cell>
+                    <Table.Cell hideBelow={"sm"}>{empl.salary}</Table.Cell>
+                    <Table.Cell hideBelow={"sm"}>{empl.birthDate}</Table.Cell>
+                    <Table.Cell>
+                      <Button
+                        margin={"5px 0"}
+                        onClick={() => {
+                          empl.id && deleteEmployee(empl.id);
+                        }}
+                        loading={isDeleting}
+                      >
+                        DELETE
+                      </Button>
+                    </Table.Cell>
                   </Table.Row>
                 ))}
               </Table.Body>
